b9003c3c345b57508e64ef9eac41255f7683132e
Jules Perna <julesrose04@gmail.com>
2024-09-01 11:32:10 -0700

    Add files via upload

---
+++ Investment code practice Textbook chapter 2.py
@@ -0,0 +1,15 @@
+#This program helps you determine an investment after ten years.
+def main():
+    print("This program helps you determine an investment after ten years")
+
+    principal=eval(input("Enter principal value :"))
+
+    apr=eval(input("Enter annual interest rate :"))
+
+    for i in range(10):
+         principal=principal*(1+apr)
+
+    print("The value in 10 years is", principal)
+main()
+
+        
